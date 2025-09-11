95739ca1fa55c19cf1b45178facef668cc45c321
cecimoura <cecimoura91@gmail.com>
2025-09-11 12:37:43 -0300

    adicionado o campo de email

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -26,3 +26,26 @@ tipoUsuario: any;
     }
   }
 }
+
+/*export class LoginComponent {
+  tipoUsuario: string = ''; // select de tipo de usuário
+  email: string = '';       // adicionado campo de email
+  senha: string = '';
+
+  constructor(private router: Router) {}
+
+  // Método de login - ainda comentado para futuras implementações do grupo
+  logar() {
+    console.log("Tipo de usuário selecionado:", this.tipoUsuario);
+    console.log("Email digitado:", this.email);
+    console.log("Senha digitada:", this.senha);
+
+    // Aqui futuramente será feita a integração com o backend para autenticação
+    // Exemplo:
+    // if (this.tipoUsuario === 'MENTOR' && this.email === 'root@sam.com' && this.senha === 'root') {
+    //   this.router.navigate(['principal/pessoas']);
+    // } else {
+    //   alert('Tipo de usuário, email ou senha inválidos!');
+    // }
+  }
+*/
