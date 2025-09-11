ce2280dfb47b31de107412e9578313d69c1c5e40
cecimoura <cecimoura91@gmail.com>
2025-09-11 13:40:47 -0300

    estrutura alterada para components/design/login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
-
+import { LoginComponent } from './components/design/login/login.component';
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   {path: '', redirectTo: 'login', pathMatch: 'full'}
