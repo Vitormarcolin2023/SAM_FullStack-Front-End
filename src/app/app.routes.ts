98597da97f948ed6f7bceb9d883f769b4b460c3e
cecimoura <cecimoura91@gmail.com>
2025-09-12 19:32:17 -0300

    ao apertar em cadastrar vai redirecionar para a tela de cadastro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,10 @@ import { LoginComponent } from './components/design/login/login.component';
 import { LandingpageComponent } from './components/design/landingpage/landingpage.component';
 import { CadastroComponent } from './components/design/cadastro/cadastro.component';
 
-
-
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'landing', component: LandingpageComponent },
   { path: '', component: LandingpageComponent },
-  { path: '**', redirectTo: '' }, // fallback (se digitar rota errada, volta pra landing)
-  { path: 'cadastro', component: CadastroComponent }
+  { path: 'cadastro', component: CadastroComponent }, // mover antes do '**'
+  { path: '**', redirectTo: '' } // fallback
 ];
