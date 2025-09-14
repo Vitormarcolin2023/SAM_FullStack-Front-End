f2fbbf72e073c7c490605e7eb2c0d65ba1b1e598
Josiane Steiernagel <josianesteiernagel@gmail.com>
2025-09-14 16:44:18 -0300

    começando o desenvolvimento do cadastro de grupos

---
+++ src/app/components/telas-internas/grupo-post/grupo-post.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-grupo-post',
+  imports: [],
+  templateUrl: './grupo-post.component.html',
+  styleUrl: './grupo-post.component.scss'
+})
+export class GrupoPostComponent {
+
+}
