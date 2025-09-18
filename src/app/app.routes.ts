a266cc1f7051b16374a360bd04685df13bc958c1
Lethicia <lethiciamsm@gmail.com>
2025-09-18 18:35:21 -0300

    comecei o visualizar projetos / não testei

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,6 +9,7 @@ import { CadastroComponent } from './components/telas-iniciais/cadastro/cadastro
 import { TelaInicialComponent } from './components/telas-internas/tela-inicial/tela-inicial.component';
 import { MentorPerfilComponent } from './components/telas-internas/mentor-perfil/mentor-perfil.component';
 import { AtivarmentorComponent } from './components/coordenacao/ativarmentor.component';
+import { VisualizarprojetosComponent } from './components/coordenacao/visualizarprojetos/visualizarprojetos.component';
 
 
 export const routes: Routes = [
@@ -20,7 +21,8 @@ export const routes: Routes = [
 
   // Páginas internas
   { path: 'tela-inicial', component: TelaInicialComponent, children: [
-    { path: 'ativar-mentor', component: AtivarmentorComponent}
+    { path: 'ativar-mentor', component: AtivarmentorComponent},
+    { path: 'visualizar-projetos', component: VisualizarprojetosComponent}
   ] },
   { path: 'mentor-perfil', component: MentorPerfilComponent },
   
---
+++ src/app/components/coordenacao/visualizarprojetos/visualizarprojetos.component.ts
@@ -0,0 +1,10 @@
+import { Component, inject } from '@angular/core';
+
+@Component({
+  selector: 'app-visualizarprojetos',
+  imports: [],
+  templateUrl: './visualizarprojetos.component.html',
+  styleUrl: './visualizarprojetos.component.scss'
+})
+export class VisualizarprojetosComponent {
+}
