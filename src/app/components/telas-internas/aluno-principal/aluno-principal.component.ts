f7ff35f0f25178dba39c280abde1ff58e013f5ed
Vitormarcolin2023 <marcolinvh@gmail.com>
2025-09-19 23:51:18 -0300

    Subindo configuração base

--- src/app/components/telas-internas/aluno-principal/aluno-principal.component.ts
+++ src/app/components/telas-internas/aluno-principal/aluno-principal.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
-
+import { NavbarTelasInternasComponent } from "../../design/navbar-telas-internas/navbar-telas-internas.component";
+import { SidebarComponent } from "../../design/sidebar/sidebar.component";
+import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-aluno-principal',
-  imports: [],
+  imports: [NavbarTelasInternasComponent, SidebarComponent, RouterOutlet],
   templateUrl: './aluno-principal.component.html',
   styleUrl: './aluno-principal.component.scss'
 })
