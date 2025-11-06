e2900d1aa51831d77a7dd1d5c2a1586408403694
Vitormarcolin2023 <marcolinvh@gmail.com>
2025-11-06 20:19:15 -0300

    Como os nomes da classe de aprovar mentorais estava muito grande, tive que refatorar o nome e consequentemente mudar os importes, isso tudo na visão do mentor

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -27,9 +27,10 @@ import { FuncionarioComponent } from './components/funcionarios/perfil/funcionar
 import { CadastroProfessorComponent } from './components/funcionarios/cadastro-professor/cadastro-professor.component';
 import { CadastroCoordenacaoComponent } from './components/funcionarios/cadastro-coordenacao/cadastro-coordenacao.component';
 import { ListarMentorComponent } from './components/funcionarios/listarmentor/listarmentor.component';
-import { AceiteDeMentoriaPrincipalComponent } from './components/telas-internas/mentor/aceite-de-mentoria-principal/aceite-de-mentoria-principal.component';
-import { AceiteDeMentoriaPerfilComponent } from './components/telas-internas/mentor/aceite-de-mentoria-principal/aceite-de-mentoria-perfil/aceite-de-mentoria-perfil.component';
-import { AceiteDeMentoriaDetaisComponent } from './components/telas-internas/mentor/aceite-de-mentoria-principal/aceite-de-mentoria-detais/aceite-de-mentoria-detais.component';
+import { AprovarMentorComponent } from './components/telas-internas/mentor/aprovar-mentor/aprovar-mentor.component';
+import { AceiteDeMentoriaPerfilComponent } from './components/telas-internas/mentor/aprovar-mentor/aceite-de-mentoria-perfil/aceite-de-mentoria-perfil.component';
+import { AceiteDeMentoriaDetaisComponent } from './components/telas-internas/mentor/aprovar-mentor/aceite-de-mentoria-detais/aceite-de-mentoria-detais.component';
+
 
 export const routes: Routes = [
   // Páginas iniciais
@@ -93,7 +94,7 @@ export const routes: Routes = [
   //Aceite de mentoria
   {
     path: 'aprovar-mentoria',
-    component: AceiteDeMentoriaPrincipalComponent,
+    component: AprovarMentorComponent,
     children: [
       {
         path: 'painel-de-mentorias',
