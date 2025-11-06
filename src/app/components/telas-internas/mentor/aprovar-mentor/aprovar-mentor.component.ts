01c902560a98b08e913fc75e808cba659d4d6814
Vitormarcolin2023 <marcolinvh@gmail.com>
2025-11-06 20:22:10 -0300

    Criação das rotas dentro dos arquivos, para seguir um padrão

--- src/app/components/telas-internas/mentor/aprovar-mentor/aprovar-mentor.component.ts
+++ src/app/components/telas-internas/mentor/aprovar-mentor/aprovar-mentor.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { SidebarComponent } from "../../../design/sidebar/sidebar.component";
+import { RouterModule } from "@angular/router";
 
 @Component({
   selector: 'app-aprovar-mentor',
-  imports: [],
+  imports: [SidebarComponent, RouterModule],
   templateUrl: './aprovar-mentor.component.html',
   styleUrl: './aprovar-mentor.component.scss'
 })
